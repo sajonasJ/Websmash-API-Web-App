71fcf73b8d1d1effba45e968f383b5a8f5496cd8
Jonas <jonas.sajonas@gmail.com>
2023-03-09 00:00:44 +1000

    task3 done

--- week2/task3/script.js
+++ week2/task3/script.js
@@ -1,37 +1,19 @@
 // Boilerplate for changing the number of items on the page using tags.
 
-// sample anonymous function
+
 let figs = document.getElementsByTagName("figure")[0].innerHTML;
-
-//TODO document.getElementById("serc").onclick = function () {
-//TODO     let searchNum = document.getElementById("searchNum").value;
-//TODO     document.getElementById("thumbnails").innerHTML ="";
-//TODO     for (let i = 0; i < searchNum; i++) {
-//TODO        document.getElementById("thumbnails").innerHTML +=
-//TODO         `<figure>${figs}</figure>`;
-//TODO     }
-//TODO }
-
-// ? sample regular function
-
-// ? document.getElementById("serc").onclick = clickHandler;
-// ? function clickHandler() {
-// ?   let searchNum = document.getElementById("searchNum").value;
-// ?  document.getElementById("thumbnails").innerHTML = "";
-// ? for (let i = 0; i < searchNum; i++) {
-// ?    document.getElementById("thumbnails").innerHTML +=
-// ?       `<figure>${figs}</figure>`;
-// ? }
-// ?}
-
-// sample anonymous function
 
 document.getElementById("serc").onclick = () => {
     let searchNum = document.getElementById("searchNum").value;
 
     document.getElementById("thumbnails").innerHTML = "";
-    for (let i = 0; i < searchNum; i++) {
+    for (let i = 0; i < searchNum && i < 10; i++) {
         document.getElementById("thumbnails").innerHTML +=
             `<figure>${figs}</figure>`;
+    }
+
+    if ( isNaN(searchNum)) {
+        document.getElementById("nav-container").innerHTML +=
+            `<a class="cat" href="">${searchNum}</a>`;
     }
 }
