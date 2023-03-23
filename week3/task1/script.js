9c133dec589ed198ca170235b05ac01a15c85317
Jonas <jonas.sajonas@gmail.com>
2023-03-23 21:09:27 +1000

    don't know

--- week3/task1/script.js
+++ week3/task1/script.js
@@ -6,22 +6,15 @@ let isCaption = false;
 let figCapArray = [];
 let figArray = [];
 $(function () {
-    // figure 0
     fig = $('figure').eq(4);
     $('#searchClick').click(search_handler);
-    // 
     for (let i = 0; i < $('figure').length; i++) {
-        // console.log($('figure'));
         figArray.push($('figure'));
-        // console.log($('figure').get());
     }
-    console.log($('figure').eq(1));
-}
-
-);
+});
 
 
-
+// clears the 
 let search_handler = () => {
     let entry = $('#searchNum').val();
     let htmlStr = '';
@@ -29,25 +22,17 @@ let search_handler = () => {
         htmlStr += `<figure>${fig}</figure>`;
     }
 
-
-
     // ! not working can access aray index but cannot access proper html figure
     // ! need to revise figz and figarray structure
 
     if (figCapArray.includes(entry)) {
         htmlStr += `<figure>${fig_cap[2]}</figure>`;
-        console.log("true");
-        // console.log(fig_cap);
         isCaption = true;
 
     } else {
         isCaption = false;
         console.log('false');
     }
-
-
-
-
 
     if (isNaN(entry) && isCaption === false) {
         $('#nav-container').append(`<a class="cat" href="">${entry}</a>`)
