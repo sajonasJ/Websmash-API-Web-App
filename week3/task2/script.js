3c85e7243512a6c670f8806eb75ff8b5a39ff435
Jonas <jonas.sajonas@gmail.com>
2023-03-25 22:35:58 +1000

    week3 task2 done

--- week3/task2/script.js
+++ week3/task2/script.js
@@ -3,9 +3,33 @@ let figure;
 let isCaption = false;
 // Start - initialisation
 $(function () {
-    figure = $("figure").html();
+    $('#thumbnails').hide();
+    $.get('/week3/data/photodata.json', function (data) {
+        display(data);
+        repeat(data)
+        $('#thumbnails').show(500);
+    });
+  
     $('#search-btn').click(search_handler);
 });
+function repeat(photodata){
+    figure = `<figure><a href="${photodata.photos[0].file}">
+    <img src="${photodata.photos[0].file}" alt="${photodata.photos[0].alternate}"/>
+    <figcaption>${photodata.photos[0].title}</figcaption></figure>`;
+    console.log(figure);
+}
+
+function display(photodata) {
+    let htmlStr = "";
+    for (let i = 0; i < photodata.photos.length; i++) {
+        htmlStr += `<figure class="scenery"><a href="${photodata.photos[i].file}">
+        <img src="${photodata.photos[i].file}" alt="${photodata.photos[i].alternate}"/>
+        <figcaption>${photodata.photos[i].title}</figcaption></figure>`
+    }
+    $('#thumbnails').html(htmlStr);
+
+}
+
 // search functions
 function search_handler() {
     let input = $('#search-input').val();
@@ -17,7 +41,7 @@ function search_handler() {
 
     } else {
         for (let i = 0; i < input; i++) {
-            htmlStr += `<figure>${figure}</figure>`;
+            htmlStr += figure;
         }
         $('#thumbnails').html(htmlStr);
     }
