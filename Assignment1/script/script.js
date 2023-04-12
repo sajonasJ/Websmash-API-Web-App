1c043f1c0b62a4e865652e288f73a15ad2c4c93e
Jonas <jonas.sajonas@gmail.com>
2023-04-12 10:42:40 +1000

    working

--- Assignment1/script/script.js
+++ Assignment1/script/script.js
@@ -2,45 +2,45 @@
 // Start - initialisation
 $(function () {
     $.get('../json/imgdata.json', function (jsonData) {
-        // getPhotos(jsonData);
-        displayMain(jsonData);
+        getPhotos(jsonData);
+        // displayMain(jsonData);
         getDestination(jsonData);
         getNavigation(jsonData);
         // displayMain(jsonData);
     });
 });
 
-function displayMain(data) {
-    let mainStr = "";
-    let photo = data.photos;
-    let dest = data.destination;
-    for (let i = 0; i < dest.length; i++) {
-        if ($(location).attr('href') === dest[i].file) {
-            mainStr += `<figure class="scenery">
-            <img src="${photo[i].file}"
-            alt="${photo[i].alternate}"/>
-            <figcaption>${photo[i].title}</figcaption>
-            </figure><p id="description">${photo[i].description}</p>`
-        }
-    }
-    console.log(mainStr)
-    console.log(dest[2].file)
-    console.log($(location).attr('href'))
-    $('#thumbnail-container').html(mainStr);
-}
-//  TODO: working code
-// function getPhotos(data) {
-//     let htmlStr = "";
+// function displayMain(data) {
+//     let mainStr = "";
 //     let photo = data.photos;
-//     for (let i = 0; i < photo.length; i++) {
-//         htmlStr += `<figure class="scenery">
-//         <img src="${photo[i].file}"
-//         alt="${photo[i].alternate}"/>
-//         <figcaption>${photo[i].title}</figcaption>
-//         </figure><p id="description">${photo[i].description}</p>`
+//     let dest = data.destination;
+//     for (let i = 0; i < dest.length; i++) {
+//         if ($(location).attr('href') === dest[i].file) {
+//             mainStr += `<figure class="scenery">
+//             <img src="${photo[i].file}"
+//             alt="${photo[i].alternate}"/>
+//             <figcaption>${photo[i].title}</figcaption>
+//             </figure><p id="description">${photo[i].description}</p>`
+//         }
 //     }
-//     $('#thumbnail-container').html(htmlStr);
+//     console.log(mainStr)
+//     console.log(dest[2].file)
+//     console.log($(location).attr('href'))
+//     $('#thumbnail-container').html(mainStr);
 // }
+//  TODO: working code
+function getPhotos(data) {
+    let htmlStr = "";
+    let photo = data.photos;
+    for (let i = 0; i < photo.length; i++) {
+        htmlStr += `<figure class="scenery">
+        <img src="${photo[i].file}"
+        alt="${photo[i].alternate}"/>
+        <figcaption>${photo[i].title}</figcaption>
+        </figure><p id="description">${photo[i].description}</p>`
+    }
+    $('#thumbnail-container').html(htmlStr);
+}
 
 function getDestination(data) {
     let desStr = "";
