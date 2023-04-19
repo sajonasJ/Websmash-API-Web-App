d4f3a81aebd440a446b911b237192de6475f9542
Jonas <jonas.sajonas@gmail.com>
2023-04-20 00:21:02 +1000

    add

--- Assignment1/script/script.js
+++ Assignment1/script/script.js
@@ -1,83 +1,96 @@
 // Global Variables
 let photos = [];
 const API_KEY = 'dc140afe3fd3a251c2fdf9dcd835be5c';
-const INTRSTNG = 'https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&per_page=10&format=json&nojsoncallback=1&api_key=' + API_KEY;
 const GETSIZES = 'https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&nojsoncallback=1&api_key=' + API_KEY + '&photo_id=';
-let mrequest = 0;
-let mrecieved = 0;
+
+let messageLength = 0;
+let messageRecieved = 0;
 // Start - initialisation
-$(function () {
-    $.get('../json/imgdata.json', function (jsonData) {
+$(() => {
+    $.get('../json/imgdata.json', (jsonData) => {
         //TODO GET JSON DATA
         getDestination(jsonData);
     });
-
-
-    $('#modal-close').click(function () {
+    $('#modal-close').click(() => {
         //TODO SET MODAL TO NONE
         $('#modal-container').css('display', 'none');
     })
-
-
-    fetch(INTRSTNG).then(function (response) {
-        //TODO FETCH FLICKR API
-        return response.json();
-    }).then(function (data) {
-        fetchPhoto(data);
-    }).catch(function (error) {
-        alert(error);
-    });
 });
 
 function fetchPhoto(data) {
     //TODO CONVERT RETURNED API DATA TO USABLE OBJECT
-    mrequest = data.photos.photo.length;
-    for (let i = 0; i < mrequest; i++) {
-        let photoObj = { 'id': data.photos.photo[i].id, 'title': data.photos.photo[i].title };
-        photos.push(photoObj);
-        getSizes(photoObj);
-    }
+    messageLength = data.photos.photo.length;
+    let photo = data.photos.photo
+    photo.forEach((photo) => {
+        // TODO SPLIT PHOTODATE
+        let photoDate = photo.datetaken.split(' ')
+        let photoObject = { 'id': photo.id, 'title': photo.title, 'date': photoDate[0] };
+        photos.push(photoObject);
+        getSizes(photoObject);
+    });
 }
 
 function getSizes(photoObj) {
     // TODO USE THE DATA OBJECT AND GET SIZES FROM API
     let getSizeReq = GETSIZES + photoObj.id;
-    mrecieved = 0;
-    $.get(getSizeReq, function (data) {
-        // TODO THIS IS INSIDE A FOR LOOP FROM fetchphoto
-        mrecieved++;
-        photoObj.file = data.sizes.size[5].source;
-        photoObj.full = data.sizes.size[data.sizes.size.length - 1].source;
-        if (mrequest === mrecieved) {
+    messageRecieved = 0;
+    $.get(getSizeReq, (data) => {
+        // TODO THIS IS INSIDE A FOR LOOP FROM fetchphoto counter to check if all photos can be displayed
+        messageRecieved++;
+        const XL = data.sizes.size.length - 1;
+        const MEDIUM = 5;
+        photoObj.file = data.sizes.size[MEDIUM].source;
+        photoObj.full = data.sizes.size[XL].source;
+        if (messageLength === messageRecieved) {
             // TODO IT COUNTS ITERATION BEFORE DISPLAYING PHOTO
-            display(photos);
+            // !Display photos
+            showImage(photos);
         }
     });
 }
 
 function getDestination(data) {
     //TODO DISPLAY NAVIGATION IMG LINKS
-    let desStr = "";
-    let dest = data.destination;
-    for (let i = 0; i < dest.length; i++) {
-        desStr += `<li><a href="${"#"}"><figure id ="fig-id"><img id="nav-img" src="${dest[i].file}" alt="${dest[i].alt}"> 
-        <figcaption id="fig-links">${dest[i].name}</figcaption></figure></a></li>`
-    }
-    $('#destination-list').html(desStr);
+    let displayList = "";
+    let destination = data.destination;
+    destination.forEach((destination) => {
+        // TODO APPEND TO DISPLAYLIST THE JSON FILEOBJECT
+        displayList += `<li><figure><img class="nav-img" data-url="${destination.url}" src="${destination.file}" alt="${destination.alt}">
+        <figcaption class="fig-links">${destination.name}</figcaption></figure></li>`;
+      });
+    $('#destination-list').html(displayList);
+
+    $('.nav-img').each(function () {
+        $(this).click(()=> {
+            // TODO CLICK EVENT FOR EACH IMAGELINKS, REMOVE VIDEO, RESET ARRAY, PASS URL OF API TO FETCH
+            $('#video').remove();
+            photos = []
+            let urLink = $(this).data('url')
+            fetch(urLink).then((response)=> {
+                return response.json();
+            }).then((data)=> {
+                fetchPhoto(data);
+            }).catch( (error)=> {
+                alert(error);
+            });
+        });
+    });
 }
-function display(data) {
-    let htmlStr = "";
-    for (let i = 0; i < data.length; i++) {
-        htmlStr += `<figure class="scenery" data-text="${data[i].title}" data-full="${data[i].full}">
-        <img id="api-img" src="${data[i].file}" alt="${data[i].title}"/>
-        <figcaption id ="fig-pic">${data[i].title}</figcaption></figure>`
-    };
 
-    $('#thumbnail-container').html(htmlStr);
+function showImage(data) {
+    // TODO DISPLAY PHOTOS ADD IMAGE DATA,MED SIZE AND FULL SIZE, TITLE AND DATE
+    let displayImage = "";
+    data.forEach((data) => {
+        // TODO PASS FETCHED API DATA TO DISPLAY PHOTOS
+        displayImage += `<figure class="scenery" data-text="${data.title}" data-full="${data.full}">
+        <img id="api-img" src="${data.file}" alt="${data.title}"/>
+        <figcaption id ="fig-pic">${data.title} Date taken: ${data.date}</figcaption></figure>`
+    });
+    $('#thumbnail-container').html(displayImage);
 
-    $('.scenery').each(function (index) {
+    $('.scenery').each(function () {
         //TODO event handler for clicking the modal, used for each loop to pass modal properties
-        $(this).click(function () {
+        $(this).click(() => {
             $('#modal-container').css('display', 'flex');
             $('#modal-content').attr('src', "");
             $('#modal-content').attr('src', $(this).attr('data-full'));
