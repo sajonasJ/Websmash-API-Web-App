7a7578554936ed5900de24f5c10bc4e6607efab0
www-data <www-data@s5284977.elf.ict.griffith.edu.au>
2023-05-15 23:19:54 +1000

    down

--- week9/task3/src/fb.js
+++ week9/task3/src/fb.js
@@ -1,54 +1,47 @@
 import $ from "jquery";
 const appID = "902755017614739";
-const myURL = "https://127.0.0.1:5500/week9/task3/dist/"
-let faceBook = `https://www.facebook.com/dialog/oauth?client_id=${appID}&redirect_uri=${myURL}&response_type=token`;
+const myURL = "https://s5284977.elf.ict.griffith.edu.au/IntelligentMediaSystems/week9/task3/dist/"
+let faceBook = `https://www.facebook.com/dialog/oauth?client_id=${appID}&redirect_uri=${myURL}&response_type=token&scope=email`;
+let accessToken;
+export let logOff = `https://www.facebook.com/logout.php?access_token=${accessToken}&confirm=1&next=${myURL}`;
+
 
 // further permission scope parameter: add to "faceBook"
 // &scope=email,public_profile
 
-// to redirect browser to a url: window.location.replace(URL);
-
-//  to direct to the facebook login page:
-// window.location.replace(faceBook);
-
-// access token must be sent with every subsequent request:
-//  if (access_token != undefined){
-//     $.post('https//graph.facebook.com/123456789/likes?access_token='+acess_token,function(response{
-//         ...
-//     });)
-// }
 
 // logout:
 // https://www.facebook.com/logout.php?access_token=CURRENT_ACCESS_TOKEN&confirm=1&next=APP_URL;
 
-export function fb_bootstrap(){
-    console.log("bootstrap on")
-    $.ajaxSetup({ cache: true });
-    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
-      FB.init({
-        appId: {appID},
-        version: 'v16.0'
-      });
-    //   $('#loginbutton,#feedbutton').removeAttr('disabled');
-      FB.getLoginStatus(updateStatusCallback);
-    });
+export function fb_bootstrap() {
+  window.location.replace(faceBook);
+  getAccessTokenFromURL();
+}
+// if (access_token != undefined) {
+//   $.post('https://graph.facebook.com/902755017614739/access_token=' + access_token, function (response) {
+//     // console.log(accessToken);
+//   });
+// }
+
+function getAccessTokenFromURL() {
+  console.log('gettingtoken')
+  const url = window.location.href;
+  const hashIndex = url.indexOf('#');
+  if (hashIndex !== -1) {
+    const paramsString = url.substring(hashIndex + 1);
+    const params = new URLSearchParams(paramsString);
+    accessToken = params.get('access_token');
+    console.log(accessToken);
+  }
 }
 
-function updateStatusCallback(response) {
-    if (response.status === 'connected') {
-        console.log("connected")
-      var uid = response.authResponse.userID;
-      var accessToken = response.authResponse.accessToken;
-    } else if (response.status === 'not_authorized') {
-        console.log("not authorized")
-        login()
-    } else {
-        console.log("not connected")
-    }
-   };
 
-function login(){
-    FB.login(function(response){
-        console.log(response);
-    })
-}
+
+// export function logOut() {
+//   window.location.replace(logOff);
+//   console.log('logoff')
+//   $("#btn").on(click, function () {
+//     $("#btn").text("Log out");
+//     console.log('click')
+//   })
+// }
